b3cfac50680c0b97116e62e7f13d7147c426a8ad
Pam <amorehyde@gmail.com>
2018-03-25 16:52:51 -0400

    updates

--- assets/js/page.js
+++ assets/js/page.js
@@ -9,17 +9,17 @@ pageEl.on("click", ".js-SubmitForm", {}, function(evt) {
 	alert('!!!!');
 });
 
-// function getName() {
-//     var name;
-//     var person = prompt("Please enter your name:", "");
-//     if (person == null || person == "") {
-//         name = "Hello stranger!";
-//     } else {
-//         name = "Hi " + person + "!";
-//     }
-//    document.getElementById("js-Name").innerHTML = name;
-// }
-// getName();
+function getName() {
+    var name;
+    var person = prompt("Please enter your name:", "");
+    if (person == null || person == "") {
+        name = "Hello stranger!";
+    } else {
+        name = "Hi " + person + "!";
+    }
+   document.getElementById("js-Name").innerHTML = name;
+}
+getName();
 
 
 });
