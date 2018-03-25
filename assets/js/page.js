9fd2308458bb04c31b0594be4ae44882797de1a6
Pam <amorehyde@gmail.com>
2018-03-25 18:04:17 -0400

    checked updates

--- assets/js/page.js
+++ assets/js/page.js
@@ -3,23 +3,51 @@ $(document).ready(function(){
 var pageEl = $("body").find(".main");
 
 pageEl.on("click", ".js-SubmitForm", {}, function(evt) {
+
 	// prevent default action
 	evt.preventDefault();
+
 	
-	alert('!!!!');
+	if (true) {
+		$(".js-SubmitForm").hide();
+		$(".results-container").slideDown("slow");
+	} else {
+		//todo: needs to validate form answers upon submission
+		alert("You have not answered all of the questions!");
+	}
+
+}).on("click", "input:checkbox", {}, function(){
+
+	//allows only one checkbox to be selected at a time
+	//todo: needs to allow two to be selected for fourth question
+	var box = $(this);
+
+	if (box.is(":checked")) {
+	    var group = "input:checkbox[name='" + box.attr("name") + "']";
+	    // the checked state of the group/box on the other hand will change
+	    // and the current value is retrieved using .prop() method
+	    $(group).prop("checked", false);
+	    box.prop("checked", true);
+	} else {
+	    box.prop("checked", false);
+	}
+
+	$("input:checkbox:not(:checked) + label").css({ opacity: 0.5 });
+	$("input:checkbox:checked + label").css({ opacity: 1 });
+
 });
 
-function getName() {
-    var name;
-    var person = prompt("Please enter your name:", "");
-    if (person == null || person == "") {
-        name = "Hello stranger!";
-    } else {
-        name = "Hi " + person + "!";
-    }
-   document.getElementById("js-Name").innerHTML = name;
-}
-getName();
+// function getName() {
+//     var name;
+//     var person = prompt("Please enter your name:", "");
+//     if (person == null || person == "") {
+//         name = "Hello stranger!";
+//     } else {
+//         name = "Hi " + person + "!";
+//     }
+//    document.getElementById("js-Name").innerHTML = name;
+// }
+// getName();
 
 
 });
