f2211195e8bbe515e2d2dcd0faa3a09bd8eb0e32
Pam <amorehyde@gmail.com>
2018-03-25 15:46:24 -0400

    name js

---
+++ assets/js/page.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+function getName() {
+    var name;
+    var person = prompt("Please enter your name:", "");
+    if (person == null || person == "") {
+        name = "Hello stranger!";
+    } else {
+        name = "Hi " + person + "!";
+    }
+   document.getElementById("js-Name").innerHTML = name;
+}
+getName();
+
+
+});
